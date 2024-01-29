df6e834154c692f00641e3a6945c2c0449620826
ahmedelsaayed <141839192+ahmedelsaayed@users.noreply.github.com>
2024-01-29 23:44:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+    document.querySelector('.btn').onclick = function() {
+        document.querySelector('h4').innerHTML = 'رقم طلبك هو 402772 وسوف نتواصل معك خلال 20 دقيقة';
+        document.querySelector('h4').style.fontSize='20px';
+        document.querySelector('h4').style.marginTop='20px';
+        
+    }
+    
